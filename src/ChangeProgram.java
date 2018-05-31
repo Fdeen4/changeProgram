d6e360bf14584677e79613b7245b8b79645719b4
GBTC440003UR <GBTC440003UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:24:42 -0400

    first commit

---
+++ src/ChangeProgram.java
@@ -0,0 +1,14 @@
+public class ChangeProgram {
+    public static void main(String[] args) {
+        int x;
+
+double y =9.5;
+        x = 5;
+
+
+      double  z = x * y;
+
+        System.out.println("The product is " + z);
+    }
+
+}
